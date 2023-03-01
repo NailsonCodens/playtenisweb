ab11b1f60530fa00c8968807e9443e54bcdaa80e
nailson <nailson.ivs@codens.com.br>
2023-03-01 11:18:25 -0300

    update

--- src/utils/statusGame.ts
+++ src/utils/statusGame.ts
@@ -1,8 +1,8 @@
 export const statusGame = {
-  available: {text: 'Disponível', color: "#000"},
+  available: {text: 'Liberada', color: "#000"},
   start: {text: 'Início', color: "#1DB83F"},
   inProgress: {text: 'Em andamento', color: "#256487"},  
   closeEnd: {text: 'Acabando', color: "#FF6433"},
   class: {text: 'Aula', color: "#4F4F4F"},
-  unavailable: {text: 'Sem uso', color: "#5D5FEF"},
+  unavailable: {text: 'Não disponível', color: "#5D5FEF"},
 }
